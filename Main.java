a542fd211e731ed46415cee1cd75e6c2bd92b718
ioregel96 <75718294+ioregel96@users.noreply.github.com>
2021-03-25 17:52:03 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
+
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+
+                MovieSearch movieSearch = new MovieSearch();
+                movieSearch.setVisible(true);
+
+            }
+        });
+    }
+}
